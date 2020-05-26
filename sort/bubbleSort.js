2b3445759204d0e3747a7f5d27e950e05cd00ab5
lichao201626 <824683639@qq.com>
2020-05-26 17:55:30 +0800

    bubble sort

--- sort/bubbleSort.js
+++ sort/bubbleSort.js
@@ -1,8 +1,19 @@
+/* function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+      }
+    }
+  }
+  return arr
+}
+ */
 function bubbleSort(arr) {
-  for (let i=0;i<arr.length;i++) {
-    for (let j=i;j<arr.length;j++) {
-      if (arr[i] > arr[j]) {
-        [arr[i], arr[j]] = [arr[j], arr[i]] 
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
       }
     }
   }
